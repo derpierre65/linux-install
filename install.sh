af06d530332131719c43e74f972dfac5773b5660
derpierre65 <admin@derpierre65.de>
2021-12-10 17:29:44 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# check if we're running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# environment variables
+LC_ALL=C.UTF-8
+PHP_PPA="ondrej/php"
+
+sudo mkdir -p /etc/apt/sources.list.d/
+
+if ! grep -q "^deb .*$PHP_PPA" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    # commands to add the ppa ...
+    add-apt-repository ppa:ondrej/php
+fi
+
+# php, mariadb-server, redis-server, 
+sudo apt -y install zsh git snapd openssl curl build-essential \
+	wget gnupg2 gnupg-agent dirmngr scdaemon hopenpgp-tools yubikey-personalization \
+	php-common php-curl php-json php-mbstring php-mysql php-xml php-zip php-swoole php-gd php-imagick \ php-redis \
+  redis-server \
+  mariadb-server \
+  flameshot \
+  ffmpeg
+
+# cryptsetup
+# pcscd
+# secure-delete
+
+# install google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+# install composer v2
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm composer-setup.php
+
+# nodejs
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# discord, slack
+sudo snap install discord
+sudo snap install slack --classic
