81d0489e03cc22868db2f27b04c31ae772187376
derpierre65 <admin@derpierre65.de>
2021-12-27 12:42:38 +0100

    Create server-install.sh

---
+++ server-install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# check if we're running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# environment variables
+LC_ALL=C.UTF-8
+PHP_PPA="ondrej/php"
+
+mkdir -p /etc/apt/sources.list.d/
+
+if ! grep -q "^deb .*$PHP_PPA" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    # commands to add the ppa ...
+    add-apt-repository ppa:ondrej/php
+fi
+
+apt
+
+# install base stuff
+apt -y install git curl build-essential wget
+
+# other programs
+INSTALL_NAMES=(
+"nodejs"
+"php"
+"redis"
+"mariadb-server"
+"php composer"
+"nginx"
+);
+INSTALL_COMMANDS=(
+"(curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -) && apt-get install -y nodejs"
+"apt -y install php-common php-curl php-json php-mbstring php-mysql php-xml php-zip php-gd php-imagick php-redis"
+"apt -y install redis-server"
+"apt -y install mariadb-server"
+"curl -sS https://getcomposer.org/installer -o composer-setup.php && php composer-setup.php --install-dir=/usr/local/bin --filename=composer && rm composer-setup.php"
+"apt -y install nginx"
+);
+
+arrayLength=${#INSTALL_COMMANDS[@]}
+for (( i=0; i<${arrayLength}; i++));
+do
+        read -e -p "Install ${INSTALL_NAMES[i]} (${i}/${arrayLength})? [Y/n] " YN
+        if ( [[ $YN == "y" || $YN == "Y" || $YN == "" ]] ) then
+                eval ${INSTALL_COMMANDS[i]}
+        fi
+done
